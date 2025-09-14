c4f564311910b8e75a2037ff29638d64bbc8ae10
DiloJan-26 <dilodilojan662000@gmail.com>
2025-09-14 22:07:27 +0530

    First state

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div>Software Engineer and <span>AI Solutionist</span></div>
-      <p>I am a recent B.Sc. Engineering graduate from the University of Moratuwa, with a strong foundation in Machine Learning, Data Science, Software Development, and DevOps. My journey, combined with hands-on projects, has allowed me to design and implement AI-driven solutions that bridge the gap between research and real-world applications.</p>
+      <div>
+        <h1>Software Engineer and <span>AI Solutionist</span></h1>
+        <p>I am a recent B.Sc. Engineering graduate from the University of Moratuwa, with a strong foundation in Machine Learning, Data Science, Software Development, and DevOps. My journey, combined with hands-on projects, has allowed me to design and implement AI-driven solutions that bridge the gap between research and real-world applications.</p>
+      </div>
     </div>
   );
 }
