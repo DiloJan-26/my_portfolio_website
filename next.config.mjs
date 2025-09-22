2ac0a8b572058711420782eda432a1d00ffa94fe
Dilojan R. <dilodilojan662000@gmail.com>
2025-09-22 21:41:53 +0530

    6th state-2 (error avoided but high loading in admin site)

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,33 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {};
+
+// // export default nextConfig;
+
+// import { withPayload } from '@payloadcms/next/withPayload'
+
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//   // Your Next.js config here
+//   experimental: {
+//     reactCompiler: false,
+//   },
+// }
 
-// export default nextConfig;
+// // Make sure you wrap your `nextConfig`
+// // with the `withPayload` plugin
+// export default withPayload(nextConfig) 
 
-import { withPayload } from '@payloadcms/next/withPayload'
+import path from 'path';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-  experimental: {
-    reactCompiler: false,
+  webpack: (config) => {
+    // Add the @payload-config alias
+    config.resolve.alias['@payload-config'] = path.resolve(
+      process.cwd(),
+      'payload.config.js'
+    );
+    return config;
   },
-}
+};
 
-// Make sure you wrap your `nextConfig`
-// with the `withPayload` plugin
-export default withPayload(nextConfig) 
+export default nextConfig;
